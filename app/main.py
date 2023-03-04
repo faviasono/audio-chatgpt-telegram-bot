af7f3ba1989ddeff85411e9b559ecac0d5667180
faviasono <andrea.faviait@gmail.com>
2023-03-04 12:44:32 +0100

    Update webhook url and whisper API

--- app/main.py
+++ app/main.py
@@ -142,9 +142,9 @@ def main():
 
 
 class DefaultConfig:
-    PORT = int(os.environ.get("PORT", 3978))
+    PORT = int(os.environ.get("PORT", 5000))
     TELEGRAM_TOKEN = os.environ.get("API_TELEGRAM", "")
-    MODE = os.environ.get("MODE", "polling")
+    MODE = os.environ.get("MODE", "webhook")
     WEBHOOK_URL = os.environ.get("WEBHOOK_URL", "")
 
     LOG_LEVEL = os.environ.get("LOG_LEVEL", "INFO").upper()
