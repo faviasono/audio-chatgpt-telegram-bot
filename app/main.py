0079b92247df32330a2ce8d8b785c27e195047dc
faviasono <andrea.faviait@gmail.com>
2023-03-04 13:49:24 +0100

    Fix bug webhook port

--- app/main.py
+++ app/main.py
@@ -143,7 +143,7 @@ def main():
 
 
 class DefaultConfig:
-    PORT = int(os.environ.get("PORT", 8443))
+    PORT = int(os.environ.get("PORT", 443))
     TELEGRAM_TOKEN = os.environ.get("API_TELEGRAM", "")
     MODE = os.environ.get("MODE", "webhook")
     WEBHOOK_URL = os.environ.get("WEBHOOK_URL", "")
