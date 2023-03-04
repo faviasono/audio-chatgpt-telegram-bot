d0687ef21dbfa7d09f11919b49a0af744483abce
faviasono <andrea.faviait@gmail.com>
2023-03-04 14:03:10 +0100

    Fix bug webhook

--- app/main.py
+++ app/main.py
@@ -127,11 +127,12 @@ def main():
 
     # Start the Bot
     if DefaultConfig.MODE == "webhook":
+        print(DefaultConfig)
         updater.start_webhook(
             listen="0.0.0.0",
             port=int(DefaultConfig.PORT),
             url_path=DefaultConfig.TELEGRAM_TOKEN,
-            webhook_url=f"https://{DefaultConfig.WEBHOOK_URL}{DefaultConfig.TELEGRAM_TOKEN}"
+            webhook_url=DefaultConfig.WEBHOOK_URL + DefaultConfig.TELEGRAM_TOKEN
         )
         #updater.bot.setWebhook(f"https://{DefaultConfig.WEBHOOK_URL}:{DefaultConfig.PORT}/{DefaultConfig.TELEGRAM_TOKEN}")
 
