b882487c7e5240767232ac0e3c4458c6f477f661
faviasono <andrea.faviait@gmail.com>
2023-03-05 19:13:27 +0100

    Fix typo /start description

--- app/main.py
+++ app/main.py
@@ -28,14 +28,13 @@ def start_command_handler(update, context):
     add_new_user(str(update.message.chat.id))
 
     start_text = """
-    Hi there, this bot allows you to query ChatGPT directly from Telegram, even with voice messages! 🤯
-    It keeps track of your past messages, remember to select /reset from the command menu to cancel the history.
+Hi there, this bot allows you to query ChatGPT directly from Telegram, even with voice messages! 🤯
+It keeps track of your past messages, remember to select /reset from the command menu to cancel the history.
     
-    Code: https://github.com/faviasono/audio-chatgpt-telegram-bot
-    Credits: @faviasono ✌🏻
-                    
-                    
-    You are ready to go 🚀
+Code: https://github.com/faviasono/audio-chatgpt-telegram-bot
+Credits: @faviasono ✌🏻
+
+You are ready to go 🚀
     """
     
     update.message.reply_text(start_text)
